ae4a3018d220d40134a585a3e6b6525a5a12a7fe
raystatic <rahul9650ray@gmail.com>
2019-10-05 19:28:35 +0530

    image asset added

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -13,13 +13,63 @@ class Home extends StatelessWidget{
 //        height: 100.0,
         margin: EdgeInsets.all(10.0), //provide margin
         padding: EdgeInsets.all(10.0), // provide padding
-        child: Text(
-            "Flight",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'Poppins', fontWeight: FontWeight.w700)
-        ),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                        "Flight 1",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'Poppins', fontWeight: FontWeight.w700)
+                    )
+                ),
+
+                Expanded(
+                    child: Text(
+                        "Flight 2",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'Poppins', fontWeight: FontWeight.w700)
+                    )
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                        "Flight 3",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'Poppins', fontWeight: FontWeight.w700)
+                    )
+                ),
+
+                Expanded(
+                    child: Text(
+                        "Flight 4",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'Poppins', fontWeight: FontWeight.w700)
+                    )
+                )
+              ],
+            ),
+            ImageAsset()
+          ],
+        )
+
       ),
     );
   }
 
+}
+
+
+class ImageAsset extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    AssetImage assetImage = AssetImage("images/abc.png");
+    Image image = Image(image: assetImage, width: 250.0, height: 250.0);
+    return Container(child: image);
+  }
 }
